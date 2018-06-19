a724b14734703f5aac3a499fe9bf93e43f70253d
robrig92 <robert.ipo@hotmail.com>
2018-06-18 22:48:30 -0500

    codigo de prueba update funcional en App.js

--- src/App.js
+++ src/App.js
@@ -9,19 +9,34 @@ import ClienteAlta from './views/cliente/ClienteAlta';
 import ClienteListado from './views/cliente/ClienteListado';
 import './style/App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-		  <div className="App">
-			  <Route exact path="/" component={Home} />
-			  <Route path="/cliente/alta" component={ClienteAlta} />
-			  <Route path="/cliente/listado" component={ClienteListado} />
+import AxiosRequest from './utils/AxiosRequest';
 
-		  </div>
-	  </Router>
-    );
-  }
+class App extends Component {
+	componentDidMount() {
+		// Ejemplo de código para realizar un update.
+		// AxiosRequest.client.post('cliente/9b98e500d3bce1b26e22bcfeef0f7e6d358bba9bb5c4bed0cb68f1feb84b9dfa', {
+		// 	_method: 'PATCH',
+		// 	enabled: 1,
+		// 	updatedAt: '2018-06-18',
+		// 	nombreContacto: 'Roberto Padilla',
+		// 	email: 'robert@mail.com',
+		// })
+		// 	.then(function(response) {
+		// 		console.log(response);
+		// 	}).catch(function(error) {
+		// 		console.log(error);
+		//  });
+	}
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<Route exact="exact" path="/" component={Home}/>
+					<Route path="/cliente/alta" component={ClienteAlta}/>
+					<Route path="/cliente/listado" component={ClienteListado}/>
+				</div>
+		</Router>);
+	}
 }
 
 export default App;
--- src/utils/AxiosRequest.js
+++ src/utils/AxiosRequest.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class AxiosRequest {
 	constructor() {
 		this.client = axios.create({
-			baseURL: 'http://localhost:5000/api/',
+			baseURL: 'http://localhost:8000/api/',
 			timeout: 3000
 		});
 	}
